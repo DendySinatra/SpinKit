2de26837a36dc68c603152c0c135c3a36e55aaa9
DendySinatra <53985401+DendySinatra@users.noreply.github.com>
2022-02-06 06:23:59 +0700

    Add files via upload

---
+++ library/src/main/java/android/spinkit/style/MultiplePulse.java
@@ -0,0 +1,22 @@
+package android.spinkit.style;
+
+import android.spinkit.sprite.Sprite;
+import android.spinkit.sprite.SpriteContainer;
+
+public class MultiplePulse extends SpriteContainer {
+    @Override
+    public Sprite[] onCreateChild() {
+        return new Sprite[]{
+                new Pulse(),
+                new Pulse(),
+                new Pulse(),
+        };
+    }
+
+    @Override
+    public void onChildCreated(Sprite... sprites) {
+        for (int i = 0; i < sprites.length; i++) {
+            sprites[i].setAnimationDelay(200 * (i + 1));
+        }
+    }
+}
---
+++ library/src/main/java/android/spinkit/style/MultiplePulseRing.java
@@ -0,0 +1,23 @@
+package android.spinkit.style;
+
+import android.spinkit.sprite.Sprite;
+import android.spinkit.sprite.SpriteContainer;
+
+public class MultiplePulseRing extends SpriteContainer {
+
+    @Override
+    public Sprite[] onCreateChild() {
+        return new Sprite[]{
+                new PulseRing(),
+                new PulseRing(),
+                new PulseRing(),
+        };
+    }
+
+    @Override
+    public void onChildCreated(Sprite... sprites) {
+        for (int i = 0; i < sprites.length; i++) {
+            sprites[i].setAnimationDelay(200 * (i + 1));
+        }
+    }
+}
